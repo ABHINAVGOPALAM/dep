a48ae65ef6076b1afee8a84271d98293bc27b826
ABHINAVGOPALAM <115263478+ABHINAVGOPALAM@users.noreply.github.com>
2024-04-30 08:01:31 +0000

    ..

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request,jsonify,render_template
 import pickle
 
-app=Flask(_name_)
+app=Flask(__name__)
 model=pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
